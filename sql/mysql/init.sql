b609ba1774f90256fbc6eddd0666c1d726d53615
Ronaldo <ronaldo-troupe@bitbucket.org>
2026-01-15 08:00:31 -0300

    fix: Fixed mysql connection error

--- sql/mysql/init.sql
+++ sql/mysql/init.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE sgs
-    CHARACTER SET utf8
-    COLLATE utf8_general_ci;
+    CHARACTER SET utf8mb4
+    COLLATE utf8mb4_unicode_ci;
 
-SET NAMES utf8;
+SET NAMES utf8mb4;
 USE sgs;
